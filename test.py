66d641b1254dec76b006d4e8b99fe42e12c689d7
Razist <squeezling@hotmail.com>
2021-10-05 23:33:58 +0800

    multibit for yl code + remove docx

--- test.py
+++ test.py
@@ -1,13 +1,14 @@
 import wave
 import numpy as np
 
+
 def ConvertToBin(input):  # convert data to binary function
     if type(input) == str:  # string input
         #print("String format detected")
         return ''.join(format(ord(x), '08b') for x in input)
     elif type(input) == bytes or type(input) == np.ndarray:  # bytes input
         #print("Byte format detected")
-        return 
+        return
     elif type(input) == int or type(input) == np.int8:  # integer input
         #print("Integer format detected")
         return format(input, '08b')
@@ -15,54 +16,64 @@ def ConvertToBin(input):  # convert data to binary function
         raise TypeError(
             "Input type not supported, please provide data in String, Bytes or Integer format")
 
+
 def encode():
-    audio = wave.open("Audio\\okay-come-on.wav",mode="rb")
-    frame_bytes = bytearray(list(audio.readframes(audio.getnframes()))) #convert to byte data
-    #print("OG:")
-    #for i in range(0,8):
+    audio = wave.open("Audio\\okay-come-on.wav", mode="rb")
+    frame_bytes = bytearray(list(audio.readframes(
+        audio.getnframes())))  # convert to byte data
+    # print("OG:")
+    # for i in range(0,8):
     #    print(format(frame_bytes[i],'08b'))
-    #print(frame_bytes)
-    string = "WOOOOOOOOOOOOOOOOOO"
-    string +="#####"
-    #string = string + int((len(frame_bytes)-(len(string)*8*8))/8) *'#'
-    #print(string)
-    binarystring=ConvertToBin(string)
-    #binarystring=list(binarystring)
+    # print(frame_bytes)
+    #string = "WOOOOOOOOOOOOOOOOOO"
+    string = open("Audio\\okay-1.wav","rb")
+    f=string.read()
+    delimiter="#####"
+    delimiter=bytearray(delimiter,'utf-8')
+    print(delimiter)
+    
+    print(string)
+    # string = string + int((len(frame_bytes)-(len(string)*8*8))/8) *'#'
+    # print(string)
+    binarystring = ConvertToBin(string)
+    # binarystring=list(binarystring)
     print(binarystring)
 
-    counter=0
-    #frame_bytes=int(frame_bytes)
+    counter = 0
+    # frame_bytes=int(frame_bytes)
     for i in range(len(frame_bytes)):
         if counter < len(binarystring):
-            #print(frame_bytes[i])
-            frame_bytes[i]=int(str(format(frame_bytes[i],'08b')[:-1])+binarystring[counter],2)
-            #print(frame_bytes[i])
-            counter+=1
-        if counter>=len(binarystring):
+            # print(frame_bytes[i])
+            frame_bytes[i] = int(str(format(frame_bytes[i], '08b')[
+                                 :-1])+binarystring[counter], 2)
+            # print(frame_bytes[i])
+            counter += 1
+        if counter >= len(binarystring):
             break
-    #print("MODDED:")
-    #for i in range(len(frame_bytes)):
-        #print(format(frame_bytes[i],'08b'))
-    #print(frame_bytes)
+    # print("MODDED:")
+    # for i in range(len(frame_bytes)):
+        # print(format(frame_bytes[i],'08b'))
+    # print(frame_bytes)
     frame_modified = bytes(frame_bytes)
-    #print("D")
-    #for i in range(len(frame_bytes)):
-        #print(format(frame_modified[i],'08b'))
-    #print(frame_modified)
-    newAudio =  wave.open('sampleStego.wav', 'wb')
+    # print("D")
+    # for i in range(len(frame_bytes)):
+    # print(format(frame_modified[i],'08b'))
+    # print(frame_modified)
+    newAudio = wave.open('sampleStego.wav', 'wb')
     newAudio.setparams(audio.getparams())
     newAudio.writeframes(frame_modified)
     newAudio.close()
     audio.close()
 
+
 def decode():
     audio = wave.open("sampleStego.wav", mode='rb')
     frame_bytes = bytearray(list(audio.readframes(audio.getnframes())))
     print(frame_bytes)
-    string=""
+    string = ""
     for i in range(len(frame_bytes)):
-        #print(format(frame_bytes[i],'08b'))
-        string+=(format(frame_bytes[i],'08b')[-1])
+        # print(format(frame_bytes[i],'08b'))
+        string += (format(frame_bytes[i], '08b')[-1])
     print(string)
     binaryPayloadFormat = [string[i:i+8]
                            for i in range(0, len(string), 8)]
@@ -74,5 +85,6 @@ def decode():
     print(payload[:-5])
     audio.close()
 
+
 encode()
-decode()
+#decode()
