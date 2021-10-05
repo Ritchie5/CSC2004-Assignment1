94e15aaac8d9c519f6c5d8969dfcfe82d4fb1f46
Razist <squeezling@hotmail.com>
2021-10-05 18:09:42 +0800

    text in audio

--- LSB_backup.py
+++ LSB_backup.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import struct
 
 
 def ConvertToBin(input):  # convert data to binary function
@@ -8,7 +9,7 @@ def ConvertToBin(input):  # convert data to binary function
         return ''.join(format(ord(x), '08b') for x in input)
     elif type(input) == bytes or type(input) == np.ndarray:  # bytes input
         #print("Byte format detected")
-        return [format(x, "08b") for x in input]
+        return 
     elif type(input) == int or type(input) == np.int8:  # integer input
         #print("Integer format detected")
         return format(input, '08b')
@@ -17,7 +18,7 @@ def ConvertToBin(input):  # convert data to binary function
             "Input type not supported, please provide data in String, Bytes or Integer format")
 
 
-#def Encode(imgname, payload):
+def Encode(imgname, payload):
     img = cv2.imread(imgname)
     #img = cv2.resize(img, (50, 50))
     print("Image size: " + str(img.shape[0]) + " by " +
@@ -58,17 +59,46 @@ def ConvertToBin(input):  # convert data to binary function
     cv2.imwrite('Encoded Image.png', imgEncode)
     return imgEncode
 
-def DynamicEncode(imgname,payload,bitPos):
-    print(bitPos)
+
+def DynamicEncode(imgname, payload, bitPos,payloadType):
+    #print(bitPos)
     img = cv2.imread(imgname)
+    #print(img)
     print("Image size: " + str(img.shape[0]) + " by " +
           str(img.shape[1]) + " pixels")  # image size(pixels)
     maxpayload = img.shape[0]*img.shape[1]*3//8
     print("Maximum payload to encode: " + str(maxpayload)+" bytes")
     # print(len(payload))
-    payload += '#####'
-    binaryPayload = ConvertToBin(payload)
-    # print(binaryPayload)
+    if payloadType==1:
+        payload += '#####'
+        binaryPayload = ConvertToBin(payload)
+        # print(binaryPayload)
+    elif payloadType==2:
+        f = open(imgname,'rb')
+        file=f.read()
+        print(file)
+        #fileToString=int(file)
+        fuck=list(struct.unpack("{}B".format(len(file)), file))
+        #fileToString+="#####"
+        print(fuck)
+        data = []
+        delimiter = [5,5,5,5,5,5,5,5,5,5]
+        
+        print(data)
+        print(fuck[0])
+        for i in range(len(fuck)):
+            data.append(ConvertToBin(fuck[i]))
+    
+        for i in range(len(delimiter)):
+            data.append(ConvertToBin(delimiter[i]))
+        #data+=delimiter
+        #print(fileToString)
+        print(data)
+        data="".join(data)
+        binaryPayload = data
+        print(binaryPayload)
+        print(len(data))
+        #payload=len(data)
     if len(payload) > maxpayload:
         print("Length of payload is to big for image of this size, please use a larger image or reduce the payload")
     index = 0
@@ -79,38 +109,41 @@ def DynamicEncode(imgname,payload,bitPos):
     for values in imgEncode:
         for pixel in values:
             b, g, r = ConvertToBin(pixel)
-            listB=list(b)
-            listG=list(g)
-            listR=list(r)
+            listB = list(b)
+            listG = list(g)
+            listR = list(r)
             for i in range(len(bitPos)):
                 if index < binaryPayloadLength:
-                    #print(index)
-                    print("Replacing pixel["+str(bitPos[i])+"] in B with "+ binaryPayload[index])
-                    print("".join(listB))
-                    listB[bitPos[i]]=binaryPayload[index]
-                    print("".join(listB))
+                    # print(index)
+                    # print("Replacing pixel["+str(bitPos[i]) +
+                    #       "] in B with " + binaryPayload[index])
+                    # print("".join(listB))
+                    listB[bitPos[i]] = binaryPayload[index]
+                    # print("".join(listB))
                     pixel[0] = int("".join(listB), 2)
                     index += 1
                 else:
-                   break
+                    break
             for i in range(len(bitPos)):
                 if index < binaryPayloadLength:
-                    #print(index)
-                    print("Replacing pixel["+str(bitPos[i])+"] in G with "+ binaryPayload[index])
-                    print("".join(listG))
-                    listG[bitPos[i]]=binaryPayload[index]
-                    print("".join(listG))
+                    # print(index)
+                    # print("Replacing pixel["+str(bitPos[i]) +
+                    #       "] in G with " + binaryPayload[index])
+                    # print("".join(listG))
+                    listG[bitPos[i]] = binaryPayload[index]
+                    # print("".join(listG))
                     pixel[1] = int("".join(listG), 2)
                     index += 1
                 else:
                     break
             for i in range(len(bitPos)):
                 if index < binaryPayloadLength:
-                    #print(index)
-                    print("Replacing pixel["+str(bitPos[i])+"] in R with "+ binaryPayload[index])
-                    print("".join(listR))
-                    listR[bitPos[i]]=binaryPayload[index]
-                    print("".join(listR))
+                    # print(index)
+                    # print("Replacing pixel["+str(bitPos[i]) +
+                    #       "] in R with " + binaryPayload[index])
+                    # print("".join(listR))
+                    listR[bitPos[i]] = binaryPayload[index]
+                    # print("".join(listR))
                     pixel[2] = int("".join(listR), 2)
                     index += 1
                 else:
@@ -141,8 +174,10 @@ def Decode(imgname):
     print(payload[:-5])
     return payload[:-5]
 
-def DynamicDecode(imgname,bitPos):
+
+def DynamicDecode(imgname, bitPos,payloadType):
     img = cv2.imread(imgname)
+    
     binaryPayload = ""
     for x in img:
         for pixel in x:
@@ -156,14 +191,33 @@ def DynamicDecode(imgname,bitPos):
 
     binaryPayloadFormat = [binaryPayload[i:i+8]
                            for i in range(0, len(binaryPayload), 8)]
-    # print(binaryPayloadFormat)
-    payload = ""
-    for byte in binaryPayloadFormat:
-        payload += chr(int(byte, 2))
-        # print(payload)
-        if payload[-5:] == "#####":
-            break
-    print(payload[:-5])
+    print(binaryPayloadFormat)
+    
+    
+    #print(payload[:-5])
+    if payloadType==1:
+        payload = ""
+        for byte in binaryPayloadFormat:
+            payload += chr(int(byte, 2))
+            #print(payload)
+            if payload[-5:] == "#####":
+                break
+        print(payload[:-5])
+    elif payloadType==2:
+        #print(payload[:-5])
+        #f=open("Decode Image.png",'wb')
+        #with open("Decode.png","wb") as xdimg:
+        #    xdimg.write(bytes(payload[:-5],'utf-8'))
+        payload=[]
+        for byte in binaryPayloadFormat:
+            payload.append(int(byte,2))
+            if payload[-10:]==[5,5,5,5,5,5,5,5,5,5]:
+                break
+        print(payload)
+        f=open("dump.txt",'w')
+        f.write(str(payload))
+        f.close
+
     return payload[:-5]
 
 
@@ -172,34 +226,49 @@ def main():
     print("NOTE: LSB only works on lossless-compression images like PNG, TIFF, and BMP")
     print("Main menu:\n1)Encode\n2)Decode")
     option = int(input("Enter in option\n"))
-    # print(option)
     if option == 1:
         print("Payload Type to encode\n1)Text\n2)Image")
-        payloadType = int(input("Enter Payload Type (1-?):"))
+        payloadType = int(input("Enter Payload Type(1-2):"))
         if payloadType == 1:
-            imgname = input("Enter image name\n")
-            payload = input("Enter secret message\n")
-            bits = int(input("Bits to replace?\n"))
-            bitPos=[]
+            print("Encoding Text in Image...")
+            imgname = input("Enter image name: ")
+            payload = input("Enter secret message: ")
+            bits = int(input("Bits to replace?: "))
+            bitPos = []
             for i in range(bits):
-                bitPosInput = int(input("Enter bit position #"+ str(i+1)+"to replace(0-7) :\n"))
+                bitPosInput = int(
+                    input("Enter bit position #" + str(i+1)+"to replace(0-7) :\n"))
                 bitPos.append(bitPosInput)
             bitPos.sort()
-            #print(bitPos)
+            # print(bitPos)
             #Encode(imgname, payload)
-            DynamicEncode(imgname,payload,bitPos)
-
+            DynamicEncode(imgname, payload, bitPos,payloadType)
+        elif payloadType == 2:
+            print("Encoding Image in Image")
+            imgname = input("Enter image name(cover image): ")
+            payload = input("Enter image name(to be hidden): ")
+            bits = int(input("Bits to replace?: "))
+            bitPos = []
+            for i in range(bits):
+                bitPosInput = int(
+                    input("Enter bit position #" + str(i+1)+" to replace(0-7) :\n"))
+                bitPos.append(bitPosInput)
+            bitPos.sort()
+            DynamicEncode(imgname,payload,bitPos,payloadType)
     elif option == 2:
         imgname = input("Enter image name\n")
+        payloadType = int(input("Payload Type?\n1)Text\n2)Image: "))
         print("Enter in key to decode\n")
-        bits=int(input("Enter number of bits replaced: "))
-        bitPos=[]
+        bits = int(input("Enter number of bits replaced: "))
+        bitPos = []
         for i in range(bits):
-            bitPosInput=int(input("Enter bit position #"+ str(i+1)+" replaced(0-7): "))
+            bitPosInput = int(
+                input("Enter bit position #" + str(i+1)+" replaced(0-7): "))
             bitPos.append(bitPosInput)
         bitPos.sort()
         print(bitPos)
-        DynamicDecode(imgname,bitPos)
+        DynamicDecode(imgname, bitPos,payloadType)
+
 
 if __name__ == "__main__":
     main()
--- test.py
+++ test.py
@@ -1,21 +1,78 @@
-from typing import no_type_check
-
-
+import wave
 import numpy as np
-import cv2
 
+def ConvertToBin(input):  # convert data to binary function
+    if type(input) == str:  # string input
+        #print("String format detected")
+        return ''.join(format(ord(x), '08b') for x in input)
+    elif type(input) == bytes or type(input) == np.ndarray:  # bytes input
+        #print("Byte format detected")
+        return 
+    elif type(input) == int or type(input) == np.int8:  # integer input
+        #print("Integer format detected")
+        return format(input, '08b')
+    else:
+        raise TypeError(
+            "Input type not supported, please provide data in String, Bytes or Integer format")
 
-img = cv2.imread("Encoded Image.png")
-count = 0
-for x in img:
-    for pixel in x:
-        print(bin(pixel[0])[-2:])
-        print(bin(pixel[1])[-2:])
-        print(bin(pixel[2])[-2:])
-        print("\n")
-        count+=1
-        if count>=10:
+def encode():
+    audio = wave.open("Audio\\okay-come-on.wav",mode="rb")
+    frame_bytes = bytearray(list(audio.readframes(audio.getnframes()))) #convert to byte data
+    #print("OG:")
+    #for i in range(0,8):
+    #    print(format(frame_bytes[i],'08b'))
+    #print(frame_bytes)
+    string = "WOOOOOOOOOOOOOOOOOO"
+    string +="#####"
+    #string = string + int((len(frame_bytes)-(len(string)*8*8))/8) *'#'
+    #print(string)
+    binarystring=ConvertToBin(string)
+    #binarystring=list(binarystring)
+    print(binarystring)
+
+    counter=0
+    #frame_bytes=int(frame_bytes)
+    for i in range(len(frame_bytes)):
+        if counter < len(binarystring):
+            #print(frame_bytes[i])
+            frame_bytes[i]=int(str(format(frame_bytes[i],'08b')[:-1])+binarystring[counter],2)
+            #print(frame_bytes[i])
+            counter+=1
+        if counter>=len(binarystring):
             break
-    if count>=10:
-        break
-            
+    #print("MODDED:")
+    #for i in range(len(frame_bytes)):
+        #print(format(frame_bytes[i],'08b'))
+    #print(frame_bytes)
+    frame_modified = bytes(frame_bytes)
+    #print("D")
+    #for i in range(len(frame_bytes)):
+        #print(format(frame_modified[i],'08b'))
+    #print(frame_modified)
+    newAudio =  wave.open('sampleStego.wav', 'wb')
+    newAudio.setparams(audio.getparams())
+    newAudio.writeframes(frame_modified)
+    newAudio.close()
+    audio.close()
+
+def decode():
+    audio = wave.open("sampleStego.wav", mode='rb')
+    frame_bytes = bytearray(list(audio.readframes(audio.getnframes())))
+    print(frame_bytes)
+    string=""
+    for i in range(len(frame_bytes)):
+        #print(format(frame_bytes[i],'08b'))
+        string+=(format(frame_bytes[i],'08b')[-1])
+    print(string)
+    binaryPayloadFormat = [string[i:i+8]
+                           for i in range(0, len(string), 8)]
+    payload = ""
+    for byte in binaryPayloadFormat:
+        payload += chr(int(byte, 2))
+        if payload[-5:] == "#####":
+            break
+    print(payload[:-5])
+    audio.close()
+
+encode()
+decode()
