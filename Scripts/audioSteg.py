5c5ee061ff2c204b58b95ba1231eb8e759db081c
nTynee <schoolzxcz@gmail.com>
2021-10-09 21:20:30 +0800

    commit all

--- Scripts/audioSteg.py
+++ Scripts/audioSteg.py
@@ -24,7 +24,7 @@ class LSB:
         return data
 
     def write_file(self, file_format, name, text):
-        with open('static\decode_output\\' + name + '_secret' + file_format, 'wb') as f:
+        with open('Scripts\\static\decode_output\\' + name + '_secret' + file_format, 'wb') as f:
             f.write(text)
     
     # ======================================= COVER OBJECT : AUDIO (WAV) =======================================
@@ -71,7 +71,7 @@ class Audio_Encode(LSB):
         ori_cover_obj = self.get_cover_audio(cover, 'cover')[0]
         cover_info = self.get_object_info(cover, 'cover')
         cover_size = cover_info[0]
-        self.outfile = 'static\encode_output\\' + cover_info[2] + "_copy" + cover_info[1]
+        self.outfile = 'Scripts\\static\encode_output\\' + cover_info[2] + "_copy" + cover_info[1]
 
         self.secret = self.get_secret_object(secret, 'secret')
         secret_info = self.get_object_info(secret, 'secret')
@@ -142,6 +142,8 @@ class Decode(LSB):
         file_format = self.from_base64(extracted_bin)[1].decode("utf-8")
         name = self.from_base64(extracted_bin)[2].decode("utf-8")
         self.write_file(file_format, name, message)
+
+        return file_format
 
 def main():
     
--- Scripts/speechRecognition.py
+++ Scripts/speechRecognition.py
@@ -1,11 +1,20 @@
 import speech_recognition as sr
+import os
 
 # Function to convert audio to text
 def audio_to_text(audioFile):
+    file_format = os.path.splitext(audioFile)[-1].lower()
+    name = os.path.basename(audioFile)
+    name = name.replace(file_format, '')
+
     r = sr.Recognizer()
     with sr.AudioFile(audioFile) as source:
         # listen for the data (load audio to memory)
         audio_data = r.record(source)
         # recognize (convert from speech to text)
         text = r.recognize_google(audio_data)
-    return text
+
+    with open('Scripts\\static\speechRecog\\' + name + '_speech_text.txt', 'w') as f:
+        f.write(text)
+    
+    return 'Scripts\\static\speechRecog\\' + name + '_speech_text.txt'
